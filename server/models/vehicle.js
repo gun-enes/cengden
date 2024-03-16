bfb3ae8a5c6e3f8382cd63cbac3d3f7ec480f11c
Enes Gün <gun.enes@metu.edu.tr>
2024-03-16 21:32:45 +0300

    commitle

--- server/models/vehicle.js
+++ server/models/vehicle.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
-
-
 const Schema = mongoose.Schema;
 const VehicleSchema = new Schema({
     title: {
-
+        type: String,
+        required: true
     },
     type: {
-
+        type: String,
+        required: true
     },
     brand: {
-
+        type: String,
+        required: true
     },
     model: {
-
+        type: String,
+        required: true
     },
-    year: {
+    /*year: {
 
     },
     color: {
@@ -41,5 +43,7 @@ const VehicleSchema = new Schema({
     },
     description: {
 
-    },
+    },*/
 })
+
+module.exports = mongoose.model('vehicle', VehicleSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const vehicle = require('../models/vehicle')
 
-//Routes
-router.get('', (req,res) => {
+//Home Route
+router.get('', async (req,res) => {
 	const locals = {
 		title: "CENGden",
 		description: "An online marketplace application"
 	};
-	res.render('index', {locals});
+	try{
+		const data = await vehicle.find();
+		res.render('index', {locals, data});
+	}catch(err){
+		console.log(err);
+	}
+
 });
+/*
+function insertVehicle(){
+	vehicle.insertMany([
+		{
+			title: "Simoştan bir öpücüğe bir araba",
+			type: "Hatchback",
+			brand: "Mitsubishi",
+			model: "SpaceStar"
+		},
+		{
+			title: "Enesten araba",
+			type: "SUV",
+			brand: "Nissan",
+			model: "Qashqai"
+		}
+	])
+}
+insertVehicle();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.get('/about', (req,res) => {
 	res.render('about');
 });
+
+
  
 module.exports = router;
